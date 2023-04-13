327979891f21c897c07541383b949d9c0bb79824
2019019043 <jyp0430@naver.com>
2023-04-14 01:34:02 +0900

    cg-ys project1 refactoring

--- project1/main_old.py
+++ project1/main_old.py
@@ -4,7 +4,12 @@ import glm
 import ctypes
 import numpy as np
 
-
+#for debug
+debug_var=45
+da=1.5
+db=.1
+dc=3.5
+printer = None
 
 # for mouse control
 g_mouse_button_left_toggle = False
@@ -13,12 +18,12 @@ g_cursor_last_xpos = 0
 g_cursor_last_ypos = 0
 
 # for orbit
-g_cam_azimuth = 0. # z축으로부터의 각임.
-g_cam_elevation = .1 # xz평면으로부터의 각임.
+g_cam_azimuth = 0. # z축으로 부터의 각임.
+g_cam_elevation = .1
 g_cam_orbit_direction_toggle = False # blender의 viewr와 같이, 처음 클릭했을 때의 elevation을 기준으로 azimuth의 회전 방향을 결정.
 
 # for pan
-g_cam_pan = glm.vec3(.0,.0,.0)
+g_cam_pan = glm.vec3(.0,.0,.0)          # pan move ( cam pos = orbit + pan )
 
 # camera's vector
 g_cam_direction = glm.vec3(.0,.0,.0)    # w vector of camera
@@ -26,15 +31,14 @@ g_cam_right = glm.vec3(.0,.0,.0)        # u vector of camera
 g_cam_up = glm.vec3(.0,.0,.0)           # v vector of camera
 
 # for zoom
-g_cam_distance = 3.6 # the distance between camera and target ## d가 0.2보다 작아지면 깨짐. 작을 수록 확대 됨.
+g_cam_distance = 3.6 # the distance between camera and target ## d가 0.2보다 작아지면 깨짐. (d=3.6 , fovy = 45)일 때 ortho 와 배율이 비슷. 작을 수록 확대 됨.
 g_ortho_mag = .15 # the magnification of lens in ortho ## 작을 수록 확대 됨.
 
-# for viewport & projection
+# for projection
 g_view_height = 10.
 g_view_width = g_view_height * 800/800
 
-# for projection
-g_P = glm.perspective(np.deg2rad(45), g_view_width/g_view_height , 0.1, 100) # fov가 30도 ~ 60도 일 때 인간의 시야와 비슷.
+g_P = glm.perspective(np.deg2rad(45), g_view_width/g_view_height , 0.1, 100) ## fovy should be in ( 0 ~ pi )
 g_P_toggle = True
 
 g_vertex_shader_src = '''
@@ -129,18 +133,65 @@ def framebuffer_size_callback(window, width, height):
     
     glViewport(0, 0, width, height)
 
+    #g_view_height = 10. # g_view_height은 항상 고정.
     g_view_width = g_view_height * width/height
     update_projection_matrix()
 
 def key_callback(window, key, scancode, action, mods):
     if key==GLFW_KEY_ESCAPE and action==GLFW_PRESS:
         glfwSetWindowShouldClose(window, GLFW_TRUE)
+    #for debug
     else:
         if action==GLFW_PRESS or action==GLFW_REPEAT:
-            if key==GLFW_KEY_V:
+            global debug_var, printer, da, db, dc, g_cam_pan
+            global  g_cam_azimuth, g_cam_elevation # for orbit
+            if key==GLFW_KEY_V: #! not for debuging
                 global g_P_toggle
                 g_P_toggle = not g_P_toggle
                 update_projection_matrix()
+            elif key==GLFW_KEY_1:
+                da -= .1
+                print(da)
+                update_projection_matrix()
+            elif key==GLFW_KEY_2:
+                da += .1
+                print(da)
+                update_projection_matrix()
+            elif key==GLFW_KEY_3:
+                debug_var -= 1
+                print(debug_var)
+                update_projection_matrix()
+            elif key==GLFW_KEY_4:
+                debug_var += 1
+                print(debug_var)
+                update_projection_matrix()
+            elif key==GLFW_KEY_5:
+                debug_var = 5
+            elif key==GLFW_KEY_J:
+                db += .1
+                print(db)
+                update_projection_matrix()
+            elif key==GLFW_KEY_H:
+                db += -.1
+                print(db)
+                update_projection_matrix()
+            elif key==GLFW_KEY_L:
+                dc += .1
+                print(dc)
+                update_projection_matrix()
+            elif key==GLFW_KEY_K:
+                dc += -.1
+                print(dc)
+                update_projection_matrix()
+            elif key==GLFW_KEY_SPACE:
+                print(printer)
+            elif key==GLFW_KEY_UP:
+                g_cam_pan += -g_cam_direction
+            elif key==GLFW_KEY_DOWN:
+                g_cam_pan += g_cam_direction
+                
+            elif key==GLFW_KEY_C:
+                print(g_cursor_last_xpos,g_cursor_last_ypos)
 
 
 def cursor_callback(window, xpos, ypos):
@@ -195,7 +246,10 @@ def cursor_callback(window, xpos, ypos):
         
         # update move
         g_cam_pan += g_cam_right * xoffset + g_cam_up * yoffset
-             
+        
+        
+        
+        
 
 def button_callback(window, button, action, mod):
     global g_mouse_button_left_toggle, g_mouse_button_right_toggle
@@ -227,12 +281,9 @@ def scroll_callback(window, xoffset, yoffset):
     new_cam_distance = g_cam_distance - yoffset * sensitivity
     if new_cam_distance >= 0.2:
         g_cam_distance = new_cam_distance
-        
-        # # projection이 ortho로 바뀌어도 비율이 유지 되도록 조정.
-        # # ! 본 과제에서는 요구되지 않은 기능이기에 주석처리함. !
-        # g_ortho_mag = g_cam_distance * 0.0417 
-        # if not g_P_toggle:
-        #     update_projection_matrix()
+        g_ortho_mag = g_cam_distance * 0.0417
+        if not g_P_toggle:
+            update_projection_matrix()
     
 
 def prepare_vao_triangle():
@@ -265,16 +316,46 @@ def prepare_vao_triangle():
 
     return VAO
 
+def prepare_vao_triangle_blue():
+    # prepare vertex data (in main memory)
+    vertices = glm.array(glm.float32,
+        # position        # color
+         0.0, 0.0, 0.0,  1.0, 1.0, 1.0, # v0
+         0.5, 0.0, 0.0,  1.0, 1.0, 1.0, # v1
+         0.0, 0.5, 0.0,  0.0, 0.0, 1.0, # v2
+    )
+
+    # create and activate VAO (vertex array object)
+    VAO = glGenVertexArrays(1)  # create a vertex array object ID and store it to VAO variable
+    glBindVertexArray(VAO)      # activate VAO
+
+    # create and activate VBO (vertex buffer object)
+    VBO = glGenBuffers(1)   # create a buffer object ID and store it to VBO variable
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)  # activate VBO as a vertex buffer object
+
+    # copy vertex data to VBO
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices.ptr, GL_STATIC_DRAW) # allocate GPU memory for and copy vertex data to the currently bound vertex buffer
+
+    # configure vertex positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), None)
+    glEnableVertexAttribArray(0)
+
+    # configure vertex colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), ctypes.c_void_p(3*glm.sizeof(glm.float32)))
+    glEnableVertexAttribArray(1)
+
+    return VAO
+
 def prepare_vao_frame():
     # prepare vertex data (in main memory)
     vertices = glm.array(glm.float32,
         # position        # color
-         -2.0, 0.0, 0.0,  1.0, 1.0, 1.0, # x-axis start
-         2.0, 0.0, 0.0,  1.0, 0.0, 0.0, # x-axis end 
-         0.0, -2.0, 0.0,  1.0, 1.0, 1.0, # y-axis start
-         0.0, 2.0, 0.0,  0.0, 1.0, 0.0, # y-axis end 
-         0.0, 0.0, -2.0,  1.0, 1.0, 1.0, # z-axis start
-         0.0, 0.0, 2.0,  0.0, 0.0, 1.0, # z-axis end 
+         -1.0, 0.0, 0.0,  1.0, 1.0, 1.0, # x-axis start
+         1.0, 0.0, 0.0,  1.0, 0.0, 0.0, # x-axis end 
+         0.0, -1.0, 0.0,  1.0, 1.0, 1.0, # y-axis start
+         0.0, 1.0, 0.0,  0.0, 1.0, 0.0, # y-axis end 
+         0.0, 0.0, -1.0,  1.0, 1.0, 1.0, # z-axis start
+         0.0, 0.0, 1.0,  0.0, 0.0, 1.0, # z-axis end 
     )
 
     # create and activate VAO (vertex array object)
@@ -309,6 +390,58 @@ def prepare_vao_box():
         [-0.2, 0.4, 0.2      ,0,1,1],
         [-0.2, 0.4, -0.2     ,0,1,1],
         [0.2, 0.4, -0.2      ,0,1,1],
+    ]
+    vertices = glm.array(np.concatenate([
+        vs[3],
+        vs[4],
+        vs[2],
+        vs[1],
+        vs[5],
+        vs[2],
+        vs[6],
+        vs[3],
+        vs[7],
+        vs[4],
+        vs[8],
+        vs[1],
+        vs[5],
+        vs[8],
+        vs[6],
+        vs[7]
+    ],dtype=np.float32))
+
+    # create and activate VAO (vertex array object)
+    VAO = glGenVertexArrays(1)  # create a vertex array object ID and store it to VAO variable
+    glBindVertexArray(VAO)      # activate VAO
+
+    # create and activate VBO (vertex buffer object)
+    VBO = glGenBuffers(1)   # create a buffer object ID and store it to VBO variable
+    glBindBuffer(GL_ARRAY_BUFFER, VBO)  # activate VBO as a vertex buffer object
+
+    # copy vertex data to VBO
+    glBufferData(GL_ARRAY_BUFFER, vertices.nbytes, vertices.ptr, GL_STATIC_DRAW) # allocate GPU memory for and copy vertex data to the currently bound vertex buffer
+
+    # configure vertex positions
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), None)
+    glEnableVertexAttribArray(0)
+
+    # configure vertex colors
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * glm.sizeof(glm.float32), ctypes.c_void_p(3*glm.sizeof(glm.float32)))
+    glEnableVertexAttribArray(1)
+
+    return VAO
+
+def prepare_vao_minibox():
+    # prepare vertex data (in main memory)
+    vs=[[], # for index to start from 1
+        [0.01, 0, 0.01         ,1,0,1],
+        [-0.01, 0, 0.01        ,1,0,1],
+        [-0.01, 0, -0.01       ,1,0,1],
+        [0.01, 0, -0.01        ,1,0,1],
+        [0.01, 0.02, 0.01       ,1,0,1],
+        [-0.01, 0.02, 0.01      ,1,0,1],
+        [-0.01, 0.02, -0.01     ,1,0,1],
+        [0.01, 0.02, -0.01      ,1,0,1],
     ]
     vertices = glm.array(np.concatenate([
         vs[3],
@@ -405,7 +538,7 @@ def main():
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE) # for macOS
 
     # create a window and OpenGL context
-    window = glfwCreateWindow(800, 800, 'project1 2019019043 박종윤', None, None)
+    window = glfwCreateWindow(800, 800, 'project1', None, None)
     if not window:
         glfwTerminate()
         return
@@ -426,9 +559,11 @@ def main():
     
     # prepare vaos
     vao_triangle = prepare_vao_triangle()
+    vao_triangle_blue = prepare_vao_triangle_blue()
     vao_frame = prepare_vao_frame()
     vao_grid = prepare_vao_grid()
     vao_box = prepare_vao_box()
+    vao_minibox = prepare_vao_minibox()
 
     # loop until the user closes the window
     while not glfwWindowShouldClose(window):
@@ -441,9 +576,12 @@ def main():
         glUseProgram(shader_program)
 
         # projection matrix
+        # use orthogonal projection (we'll see details later)
         P = g_P
 
-        # view matrix        
+        #TODO  Zoom 기능 구현
+        # view matrix
+        
         cam_target = glm.vec3(0,0,0) + g_cam_pan
         cam_orbit = g_cam_distance * glm.vec3(
             np.cos(g_cam_elevation) * np.sin(g_cam_azimuth),
@@ -453,17 +591,22 @@ def main():
         cam_pos = cam_orbit + g_cam_pan
         up = glm.vec3(.0, .1, .0) if np.rad2deg(g_cam_elevation) < 90 or np.rad2deg(g_cam_elevation) > 270 else glm.vec3(.0, -1, .0)
         
-        # camera's vector
         global g_cam_direction,g_cam_right,g_cam_up
         g_cam_direction = glm.normalize(cam_pos-cam_target) # actually opposite direction
         g_cam_right = glm.normalize(glm.cross(up,g_cam_direction))
         g_cam_up = glm.normalize(glm.cross(g_cam_direction,g_cam_right))
         
+        #cam_zoom = -g_cam_direction * g_cam_zoom
+        
+        #! for debug
+        global printer
+        printer=("cam_pos: ",cam_pos,"cam_target: ",cam_target)
+        
         V = glm.lookAt(cam_pos, cam_target, up)
         
-        # drawing part
         
-        ## current frame: P*V*I (now this is the world frame)
+        ##
+        # current frame: P*V*I (now this is the world frame)
         I = glm.mat4()
         MVP = P*V*I
         glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
@@ -478,15 +621,36 @@ def main():
         
         # draw current box
         glBindVertexArray(vao_box)
-        glDrawArrays(GL_TRIANGLE_STRIP, 0, 16)
+        #glDrawArrays(GL_TRIANGLE_STRIP, 0, 16)
+        glDrawArrays(GL_LINE_STRIP, 0, 16)
         
-        ## animating
+        ###### mini box for focus
+        M=glm.translate(g_cam_pan)
+        MVP = P*V*M
+        glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
+        # draw current minibox
+        glBindVertexArray(vao_minibox)
+        glDrawArrays(GL_TRIANGLE_STRIP, 0, 16)
+
+        ##
+        # animating
         t = glfwGetTime()
 
+        # rotation
+        th = np.radians(t*90)
+        R = glm.rotate(th, glm.vec3(0,0,1))
+
         # tranlation
-        T = glm.translate(glm.vec3(np.sin(t), .2, 0.4))
+        T = glm.translate(glm.vec3(np.sin(t), .2, 0.2))
+
+        # scaling
+        S = glm.scale(glm.vec3(np.sin(t), np.sin(t), np.sin(t)))
 
         M = T
+        # M = T
+        # M = S
+        # M = R @ T
+        # M = T @ R
 
         # current frame: P*V*M
         MVP = P*V*M
@@ -495,6 +659,19 @@ def main():
         # draw triangle w.r.t. the current frame
         glBindVertexArray(vao_triangle)
         glDrawArrays(GL_TRIANGLES, 0, 3)
+
+        # draw current frame
+        glBindVertexArray(vao_frame)
+        glDrawArrays(GL_LINES, 0, 6)
+
+        ## draw triangle_blue
+        M = glm.translate(glm.vec3(np.sin(t), .2, -0.2))
+        MVP = P*V*M
+        glUniformMatrix4fv(MVP_loc, 1, GL_FALSE, glm.value_ptr(MVP))
+        glBindVertexArray(vao_triangle_blue)
+        glDrawArrays(GL_TRIANGLES, 0, 3)
+        glBindVertexArray(vao_frame)
+        glDrawArrays(GL_LINES, 0, 6)
 
         # swap front and back buffers
         glfwSwapBuffers(window)
